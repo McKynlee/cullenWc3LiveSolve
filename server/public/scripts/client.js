11e9da6a3d58f947666b5e527fec6e9636ae00ab
Dev Jana <devjana@Devs-MBP-2.domain>
2021-02-15 09:43:06 -0600

    front end basics

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,21 @@
 $( document ).ready( onReady );
 
 function onReady(){
-    console.log( 'JQ' );
+    // click handlers
+    // elements on screen when page loads
+    $( '#createTaskButton' ).on( 'click', createTask );
+    // elements dynamically created
+    $( '#tasksOut' ).on( 'click', '.taskName', completeTask );
+    $( '#tasksOut' ).on( 'click', '.deleteTaskButton', deleteTask );
 } // end onReady
+function completeTask(){
+    console.log( 'in completeTask' );
+} // end completeTask
+
+function createTask(){
+    console.log( 'in createTask' );
+} // end createTask
+
+function deleteTask(){
+    console.log( 'in deleteTask' )
+} // end deleteTask
