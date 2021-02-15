b1a99d7e4c917f516937c3517651fdab67c69f1e
Dev Jana <devjana@Devs-MBP-2.domain>
2021-02-15 10:53:39 -0600

    end of lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,15 +12,57 @@ function onReady(){
 } // end onReady
 
 function completeTask(){
-    console.log( 'in completeTask' );
+    const myId = $( this ).data( 'id' );
+    console.log( 'in completeTask:', myId );
+    $.ajax({
+        type: 'PUT',
+        url: '/task/' + myId
+    }).then( function( response ){
+        console.log( 'back from PUT:', response );
+        getTasks();
+    }).catch( function( err ){
+        console.log( err );
+        alert( 'sedfgdf' )
+    }) //end ajax
 } // end completeTask
 
 function createTask(){
     console.log( 'in createTask' );
+    // get user input put in an object
+    let objectToSend = {
+        name: $( '#taskNameIn' ).val()
+    } // end objectToSend
+    // send to server via ajax POST
+    $.ajax({
+        type: 'POST',
+        url: '/task',
+        data: objectToSend
+    }).then( function( response ){
+        // update DOM
+        getTasks();
+    }).catch( function( err ){
+        // pop alerts
+        alert( 'uh uh...' );
+        console.log( err );
+    }) // end ajax
 } // end createTask
 
 function deleteTask(){
-    console.log( 'in deleteTask' )
+    const myId = $( this ).data( 'id' );
+    console.log( 'in deleteTask:', myId );
+    // ajax DELETE to server w/ id
+    $.ajax({
+        type: 'DELETE',
+        url: '/task/' + myId
+    }).then( function( response ){
+        // update DOM
+        console.log( 'back from DELETE:', response );
+        getTasks();
+    }).catch( function( err ){
+        // catch any errors
+        alert( 'problem!' );
+        console.log( err );
+    })
 } // end deleteTask
 
 function getTasks(){
@@ -31,8 +73,20 @@ function getTasks(){
         url: '/task'
     }).then( function( response ){
         console.log( 'back from GET:', response );
+        // empty the ul element
+        let el = $( '#tasksOut' );
+        el.empty();
         // loop through response
-        // display each task on DOM
+        for( let i=0; i< response.length; i++ ){
+            // display each task on DOM
+            let appendText = `<li><span class="taskName`;
+            if( response[ i ].complete ){
+                appendText += ` complete`; 
+            }
+            appendText += `" data-id="${ response[ i ].id }">${ response[ i ].name }</span>`;
+            appendText += `<button class="deleteTaskButton" data-id="${ response[ i ].id }">Delete</button></li>`;
+            el.append( appendText );
+        } //end for
     }).catch( function( err ){
         console.log( err );
         alert( 'nope' );
--- server/routes/todo.route.js
+++ server/routes/todo.route.js
@@ -2,9 +2,15 @@ const express = require( 'express' );
 let router = express.Router();
 const pool = require( '../modules/pool' );
 
-router.delete( '/', ( req, res )=>{
-    console.log( 'in /task DELETE:' );
-    res.send( 'ribbet' );
+router.delete( '/:id', ( req, res )=>{
+    console.log( 'in /task DELETE:', req.params );
+    let queryString = 'DELETE FROM task WHERE id=$1';
+    pool.query( queryString, [ req.params.id ] ).then( ( results )=>{
+        res.sendStatus( 200 );
+    }).catch( ( err )=>{
+        console.log( err );
+        res.sendStatus( 400 )
+    }) //end query
 }) // end DELETE
 
 router.get( '/', ( req, res )=>{
@@ -23,12 +29,24 @@ router.get( '/', ( req, res )=>{
 
 router.post( '/', ( req, res )=>{
     console.log( 'in /task POST:', req.body );
-    res.send( 'woof' );
+    let queryString = 'INSERT INTO "task" (name) values ($1)';
+    pool.query( queryString, [ req.body.name ] ).then( ( result )=>{
+        res.sendStatus( 201 );
+    }).catch( ( err )=>{
+        console.log( err );
+        res.sendStatus( 400 );
+    }) // end query
 }) // end POST
 
-router.put( '/', ( req, res )=>{
-    console.log( 'in /task PUT:' );
-    res.send( 'chirp' );
+router.put( '/:id', ( req, res )=>{
+    console.log( 'in /task PUT:', req.params.id );
+    let queryString = 'UPDATE "task" SET "complete"=true WHERE id=$1';
+    pool.query( queryString, [ req.params.id ] ).then( ( results )=>{
+        res.sendStatus( 200 );
+    }).catch( ( err )=>{
+        console.log( err );
+        res.sendStatus( 400 )
+    } ) // end query;
 }) // end PUT
 
 module.exports = router;
